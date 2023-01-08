f1d00960a21ec265ced299fcb7d2e04dafbf4ad3
Simon Wicki <simon.wicki@justwatch.com>
2023-01-08 15:41:55 +0100

    chore: output

---
+++ docs/_next/static/_0EAiYI2j-uHfFlQJ-CvQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/664-cf6bb12c7cbf883d.js","static/chunks/pages/index-be01eccc9b57ef16.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
